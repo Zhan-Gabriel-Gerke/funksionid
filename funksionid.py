84c933cea31307d6f0330e556640a3aff0f8fe0e
opilane <opilane@HP-CZC6297B92>
2021-11-10 12:25:05 +0200

    funksionid

--- funksionid.py
+++ funksionid.py
@@ -1,4 +1,15 @@
+from module1 import *
 while True:
     print("funksioind".center(50,"+"))
+    print("arithmetic- A,\nis_year_leap-Y")
+    print("square")
     v=input("arithmetic- A")
     if v.upper()=="A":
+        a=float(input("esimene arv"))
+        b=float(input("teine arv"))
+        sym=input("Tehe:")
+        rezult=arithmetic(a,b,sym)
+        print(rezult)
+    elif v.upper()=="Y":
+        is_year_leap()
+        rezolut=is_year_leap((int(input("sisesta aasta"))))
--- module1.py
+++ module1.py
@@ -1,1 +1,53 @@
-
+def arithmetic(a: float,b:float,c:str):
+    """Lihtne kalkulaator
+    +-liitmine
+    --lahutamine
+    *-korrutamine
+    /-jagamine
+    :param float a: Esimene arv
+    :param float b: Teine arv
+    :param str c: Arimeetiline tehing
+    :rtype float:"""
+    if c=="+":
+        r=a+b
+    elif c=="-":
+        r=a-b
+    elif c=="*":
+        r=a*b
+    elif c==":":
+        if b!=0:
+            r=a/b
+        else:
+            print("Div0")
+            r=0.0
+    else:
+        print("viga")
+    print(r)
+    return r
+def is_year_leap(aasta: int):
+    """Liigaasta leidmine
+    Tagastab true kui aasta on liigasta ja Flase kui ei ole 
+    :parem int aasta: Aasata number 
+    :rtype bool: Funksionid vastus loogilised formaadis
+    """
+    if aasta%4==0:
+        vastus=True
+    else:
+        vastus=False
+    return vastus 
+def square():
+    """
+    Она вычисляет переметр квадрата, площадь квадрата, диогональ квадрата
+    """
+    a=float(input("sisesta a"))
+    p=a*4
+    s=a**2
+    f=a.sqrt+a.sqrt
+    print("p",p)
+    print("s",s)
+    print("диагональ",f)
+    return a
+    return p
+    return s
+    return f
+    
